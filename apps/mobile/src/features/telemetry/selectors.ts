00dd49628e561ab6e60fac267f8f8852f95c6508
UL Service Account <hello-happy-puppy@users.noreply.github.com>
2023-11-16 18:15:18 +0000

    Promote commits from private repo to public repo

--- apps/mobile/src/features/telemetry/selectors.ts
+++
@@ -1,7 +0,0 @@
-import { MobileState } from 'src/app/reducer'
-
-export const selectLastBalancesReport = (state: MobileState): number | undefined =>
-  state.telemetry.lastBalancesReport
-
-export const selectLastBalancesReportValue = (state: MobileState): number | undefined =>
-  state.telemetry.lastBalancesReportValue
